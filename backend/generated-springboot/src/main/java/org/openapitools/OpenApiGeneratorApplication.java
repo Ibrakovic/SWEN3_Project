443188f098cc35f8cc75461bda1de39be3348623
Ibrakovic <if22b092@technikum-wien.at>
2024-10-14 18:38:35 +0200

    Hotfix Database

--- backend/generated-springboot/src/main/java/org/openapitools/OpenApiGeneratorApplication.java
+++ backend/generated-springboot/src/main/java/org/openapitools/OpenApiGeneratorApplication.java
@@ -6,15 +6,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(
-    nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+        nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+)
+@EnableJpaRepositories(
+        basePackages = {"org.openapitools.repository"}
+)
+@EntityScan(
+        basePackages = {"org.openapitools.model"}
 )
 @ComponentScan(
-    basePackages = {"org.openapitools", "org.openapitools.api" , "org.openapitools.configuration"},
-    nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+        basePackages = {"org.openapitools", "org.openapitools.api" , "org.openapitools.configuration", "org.openapitools.repository", "org.openapitools.service"},
+        nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
 )
 public class OpenApiGeneratorApplication {
 
@@ -22,7 +29,7 @@ public class OpenApiGeneratorApplication {
         SpringApplication.run(OpenApiGeneratorApplication.class, args);
     }
 
-    @Bean(name = "org.openapitools.OpenApiGeneratorApplication.jsonNullableModule")
+    @Bean
     public Module jsonNullableModule() {
         return new JsonNullableModule();
     }
